68ac91b443da3d377cf7663ae371977159c4c2e5
Matt <39172048+magiccreb@users.noreply.github.com>
2023-02-13 17:03:38 -0500

    GenerateSW imported for webpack.config.js and implemented in plugins. CSS, image, and babel rules plugged into module. Pending manifest implementation.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// added GenerateSW for service worker
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -17,17 +18,42 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // package doesn't have mini-css-extract-plugin so that may not be in use here
     plugins: [
       // added HtmlWebpackPlugin
       new HtmlWebpackPlugin({
         title: "JATE",
         template: "./index.html",
-      })
+      }),
+      // plugin for GenerateSW
+      new GenerateSW({
+        swDest: "./service-worker.js"
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader assuming no mini-css-extract-plugin
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // image rules
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        // babel
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
